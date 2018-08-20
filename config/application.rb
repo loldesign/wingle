15fef385d7027996e636b4cb71c5f124aa7ab604
Yuki Finamore <yuki@loldesign.com>
2018-08-20 18:12:51 -0300

    Styles of devise sessions, header conditions and preview layout and devise/simple_form translations

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Wingle
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.enforce_available_locales = true
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.locale = :'pt-BR'
   end
 end
