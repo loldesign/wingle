699ee1ba11f225365f5cf92fad97b33bcc3667ac
Yuki Finamore <yuki@loldesign.com>
2018-08-30 18:13:54 -0300

    Styles of cadidate register steps

--- app/controllers/candidate/steps_controller.rb
+++ app/controllers/candidate/steps_controller.rb
@@ -4,4 +4,28 @@ class Candidate::StepsController < ApplicationController
     @header_options = {style: :with_logo_back_button}
   end
 
+  def welcome_message
+    @header_options = {style: :with_logo_back_button, back_button: false}
+  end
+
+  def first
+    @header_options = {style: :with_logo_back_button}
+  end
+
+  def second
+    @header_options = {style: :with_logo_back_button}
+  end
+
+  def third
+    @header_options = {style: :with_logo_back_button}
+  end
+
+  def fourth
+    @header_options = {style: :with_logo_back_button}
+  end
+
+  def fifth
+    @header_options = {style: :with_logo_back_button}
+  end
+
 end
