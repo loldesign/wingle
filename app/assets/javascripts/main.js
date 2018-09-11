c7b82cacac27cf9c303a27f0b82bac525dee95a0
Guilherme Y. Hatano <guilherme@loldesign.com.br>
2018-09-11 18:37:34 -0300

    add cellphone mask to candidate/complete-register

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,4 +7,15 @@ $(document).on('ready page:change', function() {
 
   // jQuery Mask
   $('.cpf').mask('000.000.000-00');
+
+  var PhoneMaskBehavior = function (val) {
+    return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+  },
+  phoneOptions = {
+    onKeyPress: function(val, e, field, options) {
+        field.mask(PhoneMaskBehavior.apply({}, arguments), options);
+      }
+  };
+
+  $('.cellphone').mask(PhoneMaskBehavior, phoneOptions);
 });
