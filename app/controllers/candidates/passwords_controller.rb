77521936c9becdf18cde1898caadf932a2aa820b
Guilherme Y. Hatano <guilherme@loldesign.com.br>
2018-09-13 15:14:37 -0300

    [WIP] add support to reset password

--- app/controllers/candidates/passwords_controller.rb
+++ app/controllers/candidates/passwords_controller.rb
@@ -7,9 +7,21 @@ class Candidates::PasswordsController < Devise::PasswordsController
   # end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    if candidate_params[:cpf].present?
+      @candidate = Candidate.find_by(cpf: candidate_params[:cpf])
+      @candidate.send_reset_password_instructions
+    else
+      @candidate = Candidate.send_reset_password_instructions(candidate_params)
+    end
+    yield @candidate if block_given?
+
+    if successfully_sent?(@candidate)
+      respond_with({}, location: after_sending_reset_password_instructions_path_for(@candidate))
+    else
+      respond_with(@candidate)
+    end
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
   # def edit
@@ -28,7 +40,12 @@ class Candidates::PasswordsController < Devise::PasswordsController
   # end
 
   # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    candidate_user_login_path if is_navigational_format?
+  end
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:email, :cpf)
+  end
 end
