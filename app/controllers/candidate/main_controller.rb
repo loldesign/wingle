ea32873834a18b60783f2c7ed3e73b40ee6a9b46
Yuki Finamore <yuki@loldesign.com>
2018-09-25 12:25:03 -0300

    Created tab bar candidate navigation on resposive screens, and front o home and more steps responsive candidate area

--- app/controllers/candidate/main_controller.rb
+++ app/controllers/candidate/main_controller.rb
@@ -1,6 +1,7 @@
 class Candidate::MainController < ApplicationController
   def home
     @header_options = {style: :dashboard_header}
+    @tab_bar        = {style: :with_tab_bar}
   end
 
   def profile
