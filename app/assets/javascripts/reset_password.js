9865f6382179a57849dac3fe9b16dd48ad1f68cd
Guilherme Y. Hatano <guilherme@loldesign.com.br>
2018-09-26 11:27:00 -0300

    update reset_password.js to restrict code to reset password page

--- app/assets/javascripts/reset_password.js
+++ app/assets/javascripts/reset_password.js
@@ -9,7 +9,7 @@ function enable(input_id) {
 }
 
 $(document).on('turbolinks:load', function() {
-  if (($("#candidate_cpf").length > 0) && ($("#candidate_email").length > 0)) {
+  if (($("#reset_password #candidate_cpf").length > 0) && ($("#reset_password #candidate_email").length > 0)) {
     // if form returns error the page will have a cpf value so we disable email
     if ($("#candidate_cpf").val().length > 0) {
       disable("#candidate_email");
