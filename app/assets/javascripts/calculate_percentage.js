cae37d9167168c5dd1bccc3a76b35ce5cd8a190a
Guilherme Y. Hatano <guilherme@loldesign.com.br>
2018-10-04 11:42:16 -0300

    add js validation and format to percentage onBlur
    on candidate experience step 5

--- app/assets/javascripts/calculate_percentage.js
+++ app/assets/javascripts/calculate_percentage.js
@@ -13,15 +13,53 @@ var calculatePercentage = function(){
       $("#candidate_experience_total_functions_percentage").val(total+"%")
     }
 
+    // format numbers to percentage
+    numbersToPercentage = function() {
+      $(".experience-percentage").each(function() {
+        if ($(this).val()) {
+          var val = $(this).val()
+          $(this).val(val+"%")
+        }
+      });
+    }
 
+    validatePercentage = function() {
+      $(".experience-percentage").each(function() {
+        if ($(this).val()) {
+          var val = parseInt($(this).val())
+          if (val > 100) {
+            $(this).addClass('warning');
+            $(this).parent().find(".error-message").removeClass("hide")
+            $(this).parent().find(".error-message").html("Máximo 100%")
+          } else {
+            $(this).removeClass('warning');
+            $(this).parent().find(".error-message").addClass("hide")
+          }
+        }
+      });
+
+      var input_total = $("#candidate_experience_total_functions_percentage")[0]
+      var val = parseInt(input_total.value)
+      console.log("input value: ", val)
+      if ((val > 100) || val < 30) {
+        $(input_total).addClass('warning');
+        $(input_total).parent().find(".error-message").removeClass("hide")
+      } else {
+        $(input_total).removeClass('warning')
+        $(input_total).parent().find(".error-message").addClass("hide")
+      }
+    }
+
+    // set numbers to percentage
+    numbersToPercentage();
+    // calculate on load
     calculateTotal();
 
     $(".experience-percentage").blur(function (event) {
       event.preventDefault();
-      //remove
-      console.log("id: ", $(this).prop("id"))
       calculateTotal();
-      var val = $(this).val()
+      validatePercentage();
+      var val = $(this).val().replace(/\D/g, '')
       $(this).val(val+"%")
     });
 
