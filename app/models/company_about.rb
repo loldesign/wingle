cb1695a1a3b220bb7de1fea9faed0f72d8583e4a
Guilherme Y. Hatano <guilherme@loldesign.com.br>
2018-10-24 17:43:08 -0300

    change checkbox on steps 2 and 4 to radio button
    add validation to company_about to prevent saving array (because
    it still possible), should be changed in future

--- app/models/company_about.rb
+++ app/models/company_about.rb
@@ -1,3 +1,19 @@
 class CompanyAbout < ApplicationRecord
 	belongs_to :company
+
+	# it will prevent saving array if view is changed
+	# we can change it to a simple integer column instead array in future
+	validate :ensure_one_sector, :ensure_one_neighborhood
+
+	def ensure_one_sector
+		if sectors.count > 1
+			errors.add(:sectors, "escolha apenas um setor")
+		end
+	end
+
+	def ensure_one_neighborhood
+		if neighborhoods.count > 1
+			errors.add(:neighborhoods, "escolha apenas uma região")
+		end
+	end
 end
