63c5eacb9278d252733d6f2ff92a742170471af0
Yuki Finamore <yuki@loldesign.com>
2018-11-05 14:27:55 -0200

    Responsive modal

--- db/schema.rb
+++ db/schema.rb
@@ -371,16 +371,6 @@ ActiveRecord::Schema.define(version: 20181101185731) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "process_selection_candidates", force: :cascade do |t|
-    t.bigint "process_selection_id"
-    t.bigint "candidate_id"
-    t.string "state"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["candidate_id"], name: "index_process_selection_candidates_on_candidate_id"
-    t.index ["process_selection_id"], name: "index_process_selection_candidates_on_process_selection_id"
-  end
-
   create_table "process_selections", force: :cascade do |t|
     t.bigint "company_id"
     t.string "owner_type"
