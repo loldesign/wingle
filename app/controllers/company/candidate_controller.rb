f71a3d6951898accce87aa27cb323b3e15c28244
Eduardo Zaghi <eduardo@loldesign.com.br>
2018-11-12 11:33:47 -0200

    [WIP] - candidate company profile page

--- app/controllers/company/candidate_controller.rb
+++ app/controllers/company/candidate_controller.rb
@@ -2,6 +2,6 @@ class Company::CandidateController < ApplicationController
   def profile
     @header_options = {style: :company_full_header}
     @candidate      =  Candidate.complete.sample
-    @manager        =  CandidateInfoManager.new(candidate: @candidate )
+    @manager        =  CandidateInfoManager.new(candidate: @candidate)
   end
 end
