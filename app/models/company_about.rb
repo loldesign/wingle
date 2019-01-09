00e9652dfaf37b4d021c7a6c16f0d5ef84681f93
Guilherme Y. Hatano <guilherme@loldesign.com.br>
2019-01-09 15:27:30 -0200

    update company/about step 4 to show neighborhoods and groups

--- app/models/company_about.rb
+++ app/models/company_about.rb
@@ -3,17 +3,11 @@ class CompanyAbout < ApplicationRecord
 
 	# it will prevent saving array if view is changed
 	# we can change it to a simple integer column instead array in future
-	validate :ensure_one_sector, :ensure_one_neighborhood
+	validate :ensure_one_sector
 
 	def ensure_one_sector
 		if sectors.count > 1
 			errors.add(:sectors, "escolha apenas um setor")
-		end
-	end
-
-	def ensure_one_neighborhood
-		if neighborhoods.count > 1
-			errors.add(:neighborhoods, "escolha apenas uma região")
 		end
 	end
 end
